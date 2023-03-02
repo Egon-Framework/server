6d2b255c11c2bc178838f43a15b0ed1c0d10b928
Daniel <djperrefort@gmail.com>
2023-03-01 19:25:13 -0500

    Switches to waitress for serving

--- egon_server/cli.py
+++ egon_server/cli.py
@@ -3,10 +3,14 @@
 from argparse import ArgumentParser
 
 from flask_alembic import Alembic
+from waitress import serve
 
 from . import __version__
 from .api import flask_app
 from .orm import __db_version__, db
+
+DEFAULT_HOST = 'localhost'
+DEFAULT_PORT = 5000
 
 
 class Parser(ArgumentParser):
@@ -28,9 +32,8 @@ class Parser(ArgumentParser):
 
         run = self.subparsers.add_parser('run')
         run.set_defaults(action=Application.run_api)
-        run.add_argument('--host', type=str, default='localhost', help='the hostname to listen on')
-        run.add_argument('--port', type=int, default=5000, help='the port of the webserver')
-        run.add_argument('--debug', action='store_true', default=False, help='enable debug mode (insecure)')
+        run.add_argument('--host', type=str, default=DEFAULT_HOST, help='the hostname to listen on')
+        run.add_argument('--port', type=int, default=DEFAULT_PORT, help='the port of the webserver')
 
 
 class Application:
@@ -55,17 +58,15 @@ class Application:
             alembic.upgrade(target=__db_version__)
 
     @staticmethod
-    def run_api(host, port, debug: bool = False) -> None:
+    def run_api(host: str = DEFAULT_HOST, port: int = DEFAULT_PORT) -> None:
         """Run the application
 
         Args:
             host: the hostname to listen on
             port: the port of the webserver
-            debug: Enable or disable debug mode
         """
 
-        Application.migrate_db()
-        flask_app.run(host=host, port=port, debug=debug, load_dotenv=False)
+        serve(flask_app, host=host, port=port)
 
     def execute(self) -> None:
         """Parse arguments and run the application"""
