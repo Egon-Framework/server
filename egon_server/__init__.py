599ff98e6bfe26eb3884e55819577d33eebe37ce
Daniel <djperrefort@gmail.com>
2023-03-04 18:32:55 -0500

    Configure application logging at init

--- egon_server/__init__.py
+++ egon_server/__init__.py
@@ -1,3 +1,9 @@
 import importlib.metadata
+from logging.config import dictConfig
+
+from . import settings
 
 __version__ = importlib.metadata.version(__package__)
+
+# Configure application logging
+dictConfig(settings.Settings().get_log_settings())
