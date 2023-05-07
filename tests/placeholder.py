a089750bb1c29005781db3f565870f9afd7d3d9c
Daniel Perrefort <djperrefort@gmail.com>
2023-05-06 20:00:05 -0400

    Adds CI for testing and release (#25)
    * Adds CI for testing and release
    
    * Allows Python 3.8+ in toml file
    
    * Adds badges to readme

---
+++ tests/placeholder.py
@@ -0,0 +1,10 @@
+"""This is a placeholder"""
+
+from unitest import Testcase
+
+
+class Placeholder(Testcase):
+    """This is a test"""
+
+    def runTest(self) -> None:
+        """This is a test"""
