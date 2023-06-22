e23c38b0725ea99e1af60adea3d59be4c9e34be0
Daniel <djperrefort@gmail.com>
2023-06-21 21:40:38 -0400

    Wraps __version__ declaration with exception handling

--- egon_server/__init__.py
+++ egon_server/__init__.py
@@ -3,7 +3,11 @@ import logging.config
 
 from .settings import SETTINGS
 
-__version__ = importlib.metadata.version(__package__)
+try:
+    __version__ = importlib.metadata.version(__package__)
+
+except importlib.metadata.PackageNotFoundError:
+    __version__ = '0.0.0'
 
 # Configure logging for the application and it's dependencies
 logging.config.dictConfig(SETTINGS.get_logging_config())
